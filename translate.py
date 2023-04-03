1d93883b5d4c60bc26b8c3322f13e36f98b05a7e
danielnv051 <danielnv0910@gmail.com>
2023-04-03 21:48:54 +0430

    complete assignment 8

--- translate.py
+++ translate.py
@@ -1,3 +1,9 @@
+def show_menu():
+    print('Welcome to my translator')
+    print('1- Translate english to persian')
+    print('2- Translate persian to english')
+    print('3- Add new word to dictionary')
+    print('4- Exit')
 
 def read_from_file():
     global words_bank
@@ -9,16 +15,27 @@ def read_from_file():
         words_bank.append(my_dict) 
     f.close()
 
+def translate_english_to_persian():
+    user_text = input("Enter your english text: ")
+    user_words = user_text.split(' ')
+
+    for user_word in user_words:
+        for word in words_bank:
+            if user_word == word['en']:
+                print(word['fa'], end=' ')
+                break
+        else:
+            print(user_word, end=' ')
+
 read_from_file()
-
-user_text = input("Enter your english text: ")
-user_words = user_text.split(' ')
-
-for user_word in user_words:
-    for word in words_bank:
-        if user_word == word['en']:
-            print(word['fa'], end=' ')
-            break
-
-
-
+while True:
+    show_menu()
+    choice = int(input('enter your choice: '))
+    if choice == 1:
+        translate_english_to_persian()
+    elif choice ==2:
+        ...
+    elif choice ==3:
+        ...
+    elif choice ==4:
+        exit(0)
